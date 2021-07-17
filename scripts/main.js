7c7d4ac1171aa82456c8a3ac0c6d81e321e8706f
Huzaifa Raghav <windowpane1712@gmail.com>
2021-07-17 20:09:28 +0800

    Add Operate function

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+function Operate(){
+    function add(a,b){
+        return a + b;
+    }
+
+    function minus(a,b){
+        return a - b;
+    }
+
+    function multiply(a,b){
+        return a * b;
+    }
+
+    function divide(a,b){
+        return a/b;
+    }
+
+    function negate(a){
+        return a == 0 ? 0 : -a;
+    }
+
+    function percent(a){
+        return a/100;
+    }
+
+    function handler(op, ...args){
+
+    }
+}
