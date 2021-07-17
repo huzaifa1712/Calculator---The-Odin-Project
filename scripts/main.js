bed3f0d27cdd5958c83a72cfb31a3517517a7281
Huzaifa Raghav <windowpane1712@gmail.com>
2021-07-18 01:00:53 +0800

    Refactor IO to be non-static

--- scripts/main.js
+++ scripts/main.js
@@ -59,15 +59,53 @@ testOperate = [
 ]
 
 
+// class IO{
+//     constructor(calculator){
+//         this.calculator = calculator;
+//         IO.buttonSetup();
+//     }
+
+//     static display = document.getElementById("display");
+//     static buttonDiv = document.getElementById("buttons");
+
+//     static buttonSetup(){
+//         const attrString = "data-value";
+//         // add event listener to buttons div, use bubbling to get the value
+//         this.buttonDiv.addEventListener('click', function(evt){
+//             let target = evt.target; // the elem that was clicked
+
+//             if(target.hasAttribute(attrString)){
+//                 console.log(target);
+//             }
+            
+//         });
+//     }
+
+//     // overwrites the current text completely with new text
+//     static changeDisplay(text){   
+//         this.display.innerText = text;
+//     }
+
+//     // appends the text directly to the current contents of display
+//     static appendDisplay(text){
+//         return this.changeDisplay(this.display.innerText + text);
+//     }
+
+//     static writeDisplay(text, overwrite){
+//         return overwrite ?  this.changeDisplay(text) : this.appendDisplay(text);
+//     }
+
+// }
+
 class IO{
-    constructor(){
-        IO.buttonSetup();
+    constructor(calculator){
+        this.calculator = calculator;
     }
 
-    static display = document.getElementById("display");
-    static buttonDiv = document.getElementById("buttons");
+    display = document.getElementById("display");
+    buttonDiv = document.getElementById("buttons");
 
-    static buttonSetup(){
+    buttonSetup(){
         const attrString = "data-value";
         // add event listener to buttons div, use bubbling to get the value
         this.buttonDiv.addEventListener('click', function(evt){
@@ -81,33 +119,36 @@ class IO{
     }
 
     // overwrites the current text completely with new text
-    static changeDisplay(text){   
+    changeDisplay(text){   
         this.display.innerText = text;
     }
 
     // appends the text directly to the current contents of display
-    static appendDisplay(text){
+    appendDisplay(text){
         return this.changeDisplay(this.display.innerText + text);
     }
 
-    static writeDisplay(text, overwrite){
+    writeDisplay(text, overwrite){
         return overwrite ?  this.changeDisplay(text) : this.appendDisplay(text);
     }
 
 }
 
-io = new IO();
+
 
 // pass in IO object with certain methods and operate object with certain methods
 // calculator object takes in Controller instead
 // calc is the Model, Controller is interface between IO methods and model
 // pass IO to controller, pass controller to calculator 
 class Calculator{
-    constructor(IOInterface, operate){
-        this.IO = IOInterface;
+    constructor(operate){
+        //this.IO = IO(this);
         this.result = 0;
         this.text = String(this.result);
         this.overwrite = true;
         this.operate = operate;
     }
 }
+
+calc = new Calculator(operate);
+io = new IO(calc);
