4c528e4bb6a5c03f00c4e23befbcf1fd67de8eb7
Huzaifa Raghav <windowpane1712@gmail.com>
2022-02-06 12:55:05 +0800

    Remove unnec. files

--- Basic Calculator_files/main.js
+++
@@ -1,7 +0,0 @@
-operate = Operate();
-// IO object uses as an inteface to DOM for input/output
-// Operate provides arithmetic logic
-
-// Calculator acts as the controller for managing state and deciding which logic and IO functions to call
-// as a result
-calc = new Calculator(operate);
